8768a7d390afa31cf0a3c9707f61afa38aa13161
Kaiserkrow <42108060+Kaiserkrow@users.noreply.github.com>
2024-03-13 18:34:02 +0800

    first commit: Sidebar
    Added sidebar to ehr

---
+++ index.js
@@ -0,0 +1,28 @@
+let isOpen = 1;
+let button = document.getElementById("open-close-btn");
+let closebtn1 = document.getElementById("close-btn1");
+let closebtn2 = document.getElementById("close-btn2");
+function openNav() {
+  let width = window.innerWidth;
+  if (isOpen == 0) {
+    button.classList.remove("x-button");
+    button.textContent = "☰";
+    button.setAttribute;
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    isOpen = 1;
+  } else if (isOpen == 1) {
+    button.textContent = "✕";
+    if (width < 600) {
+      closebtn1.classList.remove("hidden");
+      closebtn2.classList.remove("hidden");
+      document.getElementById("mySidebar").style.width = "100vw";
+    } else {
+      closebtn1.classList.add("hidden");
+      closebtn2.classList.add("hidden");
+      document.getElementById("mySidebar").style.width = "150px";
+    }
+    document.getElementById("main").style.marginLeft = "150px";
+    isOpen = 0;
+  }
+}
